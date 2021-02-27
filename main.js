a6b2baa6822c9091ece7d06e75bf435f4f881c3c
bennKaoutar <benn.kaoutar@gmail.com>
2021-02-27 10:59:07 +0100

    changes in main and renderer in order to send msg to webView

--- main.js
+++ main.js
@@ -5,6 +5,32 @@ const { app, BrowserWindow, ipcMain} = require('electron')
     event.reply('reply', 'from main')
 
   })*/
+  var webview = document.getElementById("myweb");
+// When everything is ready, trigger the events without problems
+  webview.addEventListener("dom-ready", function() {
+  // Show devTools if you want
+  //webview.openDevTools();
+  console.log("DOM-Ready, triggering events !");
+  
+  // Aler the scripts src of the website from the <webview>
+  webview.send("request");
+  
+  // alert-something
+  webview.send("alert-something", "Hey, i'm alerting this.");
+
+      // change-text-element manipulating the DOM
+      webview.send("change-text-element",{
+        id: "myelementID",
+        text: "My text"
+    });
+});
+
+  // Process the data from the webview
+webview.addEventListener('ipc-message',function(event){
+  console.log(event);
+  console.info(event.channel);
+});
+
 
 function createWindow () {
   const win = new BrowserWindow({
--- renderer.js
+++ renderer.js
@@ -1,20 +1,52 @@
-console.warn('loaded');
+const {ipcRenderer} = require('electron');
+
+//console.warn('loaded');
 window.onload=()=>{
     const webview = document.querySelector("#myweb")
     const loadiing = document.querySelector(".loading")
     
     webview.addEventListener("did-start-loading", ()=>{
-        loadiing.innerHTML='loading..'
+        console.warn('loading..');
     })
 
     webview.addEventListener("did-stop-loading", ()=>{
         loadiing.innerHTML='stopped!'
     })
 
-    webview.addEventListener("dom-ready", ()=>{
+    webview.addEventListener("dom-ready",()=>{
         console.warn("dom is charged");
+        
     })
 
-    
+        // Do something according to a request of your mainview
+    ipcRenderer.on('request', function(){
+        ipcRenderer.sendToHost(getScripts());
+    });
+    //webview.getElementsByClassName("kaoutarbtn").value
+
+    ipcRenderer.on("alert-something",function(event,data){
+        alert(data);
+    });
+
+    ipcRenderer.on("change-text-element",function(event,data){
+        // the document references to the document of the <webview>
+        document.getElementById(data.id).innerHTML = data.text;
+    });
+ 
+   /**
+ * Simple function to return the source path of all the scripts in the document
+ * of the <webview>
+ *
+ *@returns {String}
+ **/
+   function getScripts(){
+       var items = [];
+       
+       for(var i = 0;i < document.scripts.length;i++){
+           items.push(document.scripts[i].src);
+       }
+       
+       return JSON.stringify(items);
+   }
 
 }
